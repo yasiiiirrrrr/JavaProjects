338bb2dbd5c32af224105d73fbe3c66a6fe0be44
Yasir <157069639+yasirrdev@users.noreply.github.com>
2024-01-19 13:04:53 +0100

    Add files via upload

---
+++ src/Banco/main.java
@@ -0,0 +1,21 @@
+package BANCO_JAVA;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Pedir datos para la creación de la cuenta
+        System.out.println("Introduce tu nombre por favor:");
+        String nombre = sc.nextLine();
+        System.out.println("Introduce tu DNI por favor:");
+        String dni = sc.nextLine();
+
+        // Crear instancia de CuentaCorriente con los datos proporcionados
+        CuentaCorriente persona = new CuentaCorriente(dni, nombre);
+
+        // Mostrar el menú
+        persona.mostrarMenu();
+    }
+}
